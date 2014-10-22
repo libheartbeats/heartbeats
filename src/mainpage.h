7877968e37e7e66e3a63dc74844b3d9908f719c6
Connor Imes <connor.imes@gmail.com>
2014-10-22 18:09:37 +0000

    Documentation/README updates

--- src/mainpage.h
+++ src/mainpage.h
@@ -9,23 +9,27 @@
 *
 * However, existing approaches to adaptive systems are largely ad hoc and often do not manage to incorporate the true performance goals of the applications they are designed to support.
 *
-* This project proposed an enabling technology for adaptive computing systems: Application Heartbeats. The Application Heartbeats framework provides a simple, standard programming interface that applications can use to indicate their performance and system software (and hardware) can use to query an application's performance.
+* This project proposed an enabling technology for adaptive computing systems: Application Heartbeats. The Application Heartbeats framework provides a simple, standard programming interface that applications can use to indicate their performance and system software (and hardware) can use to query an application's performance. The latest version complements performance monitoring with the ability to also monitor accuracy and power.
 *
 * <hr>
 * @section download How to Download the Application Heartbeats
-* If you want to download the latest stable release of the Heartbeats framework, visit:
+* You can get the latest 2.x version of the Heartbeats framework from:
+*
+* <pre class="wiki">https://github.com/libheartbeats/heartbeats</pre>
+*
+* The original 1.x versions are still available at:
 *
 *  <pre class="wiki">http://code.google.com/p/heartbeats/downloads/list</pre>
 *
-* If you want to checkout the Heartbeats repository and to create a working copy of it on your local machine
+* To checkout the Heartbeats 1.x repository and create a working copy on your local machine:
 *
 * <pre class="wiki">svn checkout https://heartbeats.googlecode.com/svn/trunk/ heartbeats --username <YourUserName> </pre>
 *
 * More information available here:  <pre class="wiki">http://code.google.com/p/heartbeats/source/checkout </pre>
 *
 * <hr>
-* @section web Application Heartbeats on the web
-* You can find the Application Heartbeats on the web using the following link:
+* @section web Application Heartbeats on the Web
+* You can find the Application Heartbeats on the web using the following links:
 * - <a href="http://www.youtube.com/user/hearbeatapi" target="_blank">Video available on YouTube</a>
 * - <a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=337587051" target="_blank">iTunes Channel</a>
 * - <a href="http://groups.google.com/group/heartbeatsapi" target="_blank">Discussion Group</a>
@@ -33,6 +37,7 @@
 * - <a href="https://groups.csail.mit.edu/carbon/heartbeats/" target="_blank">Hearbeats code documentation</a>
 *
 * <hr>
+* @authors Connor Imes
 * @authors Henry Hoffmann
 * @authors Jonathan Eastep
 * @authors Marco D. Santambrogio
